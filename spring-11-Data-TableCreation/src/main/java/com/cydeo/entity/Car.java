3a573b838547a49ef77984329cd480c7238540df
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-09 13:53:08 -0700

    spring-11-Data-TableCreation

---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -0,0 +1,27 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String make;
+    private String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+}
