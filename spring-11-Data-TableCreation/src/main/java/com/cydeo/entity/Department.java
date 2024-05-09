b38abcef3629603dc3544f8fcbd654e18ad5219d
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-09 13:54:11 -0700

    spring-11-Data-TableCreation

---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Department.java
@@ -0,0 +1,18 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Department extends BaseEntity{
+
+    private String department;
+    private String division;
+
+}
---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private int salary;
+
+
+
+}
