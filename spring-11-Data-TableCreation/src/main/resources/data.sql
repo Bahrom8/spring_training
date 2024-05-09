3f82cd164ddb293f0cdbacd4f9cd620b315d0999
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-09 13:53:29 -0700

    spring-11-Data-TableCreation

---
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into Staffs(id, name)
+values(1, 'John');
+
+insert into students(first_name, last_name, email)
+values('Mike', 'Smith', 'mike@gmail.com');
+
+insert into students(first_name, last_name, email)
+values('John', 'Evans', 'john@gmail.com');
