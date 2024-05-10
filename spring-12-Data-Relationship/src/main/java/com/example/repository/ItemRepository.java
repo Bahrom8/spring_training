27946b04a2d0ebbb93a28bb75e592da12fb05edf
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-10 12:10:01 -0700

    spring-12-Data-Relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/example/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+
+}
