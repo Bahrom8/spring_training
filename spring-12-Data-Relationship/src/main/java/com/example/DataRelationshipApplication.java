c3304301dccab429af3cec7ecf7f95a6d5468d87
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-10 12:09:20 -0700

    spring-12-Data-Relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/example/DataRelationshipApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DataRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataRelationshipApplication.class, args);
+    }
+
+}
