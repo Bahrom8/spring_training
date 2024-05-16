2561c94c594029020d9bd7daac0550234076779e
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-15 20:15:23 -0700

    spring-13-Data-Cinema-Lab

--- spring-13-Data-Cinema-Lab/src/main/java/com/example/repository/CinemaRepository.java
+++ spring-13-Data-Cinema-Lab/src/main/java/com/example/repository/CinemaRepository.java
@@ -1,8 +1,14 @@
 package com.example.repository;
 
+import com.example.entity.Account;
 import com.example.entity.Cinema;
+import com.example.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
@@ -10,20 +16,26 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to get cinema with a specific name
+    Optional<Cinema> findAllByName(String name);
 
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
+    List<Cinema> findTop3BySponsoredNameContainingOrderBySponsoredName(String sponsoredName);
 
 
     //Write a derived query to list all cinemas in a specific country
+    List<Cinema> findAllByLocationCountry(String country);
 
 
     //Write a derived query to list all cinemas with a specific name or sponsored name
+    List<Cinema> findAllByNameOrSponsoredName(String name, String sponsoredName);
 
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT a FROM Cinema a WHERE a.id=1")
+    List<Account> cinemas();
 
 
     // ------------------- Native QUERIES ------------------- //
