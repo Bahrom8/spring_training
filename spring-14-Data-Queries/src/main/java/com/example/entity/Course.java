836cc632720b4462e31828697ab1b68a4f16b16a
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-15 20:16:31 -0700

    spring-14-Data-Queries

---
+++ spring-14-Data-Queries/src/main/java/com/example/entity/Course.java
@@ -0,0 +1,27 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "COURSES")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "CATEGORY")
+    private String category;
+    @Column(name = "RATING")
+    private String rating;
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+
+
+}
