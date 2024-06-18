0e978170a97ac33a2ceaa4067f84407f9be3e859
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-06-18 15:51:16 -0700

    REST API

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,31 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //@Controller + @ResponseBody
+@RequestMapping("/courses/api/v1")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses() {
+
+        return courseService.getCourses();
+    }
+
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") long courseId) {
+        return courseService.getCourseById(courseId);
+    }
+
+}
